7e82437efa32bfe8d7deee5d37707f1286dcc306
anitahaurie <a_hauri@encs.concordia.ca>
2016-03-29 18:35:29 -0400

    Memory Man

--- A3/Project4/Project4/MemoryManager.cpp
+++ A3/Project4/Project4/MemoryManager.cpp
@@ -17,15 +17,23 @@ MemoryManager* MemoryManager::getInstance()
 
 void MemoryManager::store(int, int)
 {
+	//check if main memory has empty slot
+	//if yes, store in main
+	//else, store in disk
 
 }
 
 void MemoryManager::release(int)
 {
-
+	//search main memory
+	//if in main, delete
+	//else, search disk
+	//release
 }
 
 void MemoryManager::lookup(int)
 {
-
+	//get in main memory
+	//if null, load LRU into main
+	//get from main
 }
